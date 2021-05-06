05994b5ab6c611b631cb70da73b71f70d466021d
Michelle Watts <watts.e.michelle@gmail.com>
2021-05-06 20:41:25 +1000

    cange to for loop and edited mapping

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,32 @@ var formEl = $('#generator');
 var nameInputEl = $('#link');
 var skillsListEl = $('#printed-links');
 
-const array1 = ["?source=fb", "?source=twitter", "?source=insta", "?source=linkedin"];
+
+// const array1 = ["?source=fb", "?source=twitter", "?source=insta", "?source=linkedin"];
+
+
+const array1 = [
+    {
+      name: 'Facebook organic',
+      source: '?source=fb',
+    },
+    {
+        name: 'Facebook paid',
+        source: '?source=fbpaid',
+    },
+    {
+        name: 'Twitter',
+        source: '?source=twitter',
+    },
+    {
+        name: 'Instagram',
+        source: '?source=insta',
+    },
+    {
+        name: 'LinkedIn',
+        source: '?source=linkedin',
+    },
+  ];
 
 // pass a function to map
 
@@ -11,32 +36,46 @@ var handleFormSubmit = function (event) {
     event.preventDefault();
 
     var nameInput = nameInputEl.val();
-    const map1 = array1.map(x => nameInput + x);
+    // const map1 = array1.map(x => nameInput + x);
 
-    console.log(map1)
-    console.log(nameInput)
+    let map1 = array1.map(obj => {
+        let rObj = {}
+        rObj["name"] = obj.name 
+        rObj["source"] = nameInput + obj.source;
+        return rObj
+     })
+     
 
-    if (!nameInput) {
-        alert('You need to put a link in this field');
-        return;
-    }
-
-    $.each(map1, function (index, value) {
+    var i;
+    for (i = 0; i < map1.length; i++) {
         var listEl = $('<li>');
-        listEl.addClass('list-group-item copy').text(value);
+        var heading = $('<h4>')
+        var link = $('<a>').addClass('copy');
+        link.text(map1[i].source)
+        heading.text(map1[i].name)
+        listEl.addClass('list-group-item copy');
         listEl.appendTo(skillsListEl);
-    });
+        heading.appendTo(listEl);
+        link.appendTo(listEl);
+    }
+   
 
-    // resets form
-    nameInputEl.val('');
+
 
 };
 
 formEl.on('submit', handleFormSubmit);
 
-function copyLink() {
-    var linkSelect = document.querySelector(".copy");
-    linkSelect.select();
-    document.execCommand("copy");
-    alert('Link copied!')
-}
+// function copyLink() {
+//     var linkSelect = document.querySelector(".copy");
+//     linkSelect.select();
+//     document.execCommand("copy");
+//     alert('Link copied!')
+// }
+
+// $(".copy").on('click', function(event){
+//     var linkSelect = document.querySelector(".copy");
+//     linkSelect.select();
+//     document.execCommand("copy");
+//     alert('Link copied!')
+// })
